79fdadc8925f28d83fad91608de80719b60041cb
luis <luis@lsuclupe>
2012-09-24 15:06:53 -0500

    Prueba

---
+++ streaming-model/src/main/java/streaming/model/EntityManagerStreaming.java
@@ -0,0 +1,22 @@
+package streaming.model;
+
+import java.util.Collection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.dao.DataAccessException;
+
+@Repository
+@Transactional
+public class EntityManagerStreaming  {
+
+	@PersistenceContext
+	private EntityManager em;
+
+
+	
+}
